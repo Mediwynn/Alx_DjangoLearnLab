1b761359dd9eb9c69a3d25ca1189a5e3098cbb14
Mediwynn M Maart <mediwynnm@gmail.com>
2024-08-16 16:49:38 +0200

    added views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,42 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Book
-from django.views.generic import DetailView
-from .models import Library
-from django.views.generic.detail import DetailView
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
-from django.contrib.auth import login
-from django.shortcuts import redirect
-from django.http import HttpResponseForbidden
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-
-
-def list_books(request):
-    # Query all books from the database
-    books = Book.objects.all()
-    
-    # Pass the books to the template
-    context = {
-        'books': books,
-    }
-    
-    return render(request, 'relationship_app/list_books.html', context)
-
-class LibraryDetailView(DetailView):
-
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add the list of books in the library to the context
-        context['books'] = self.object.books.all()  # Assuming 'books' is the related name for the ManyToManyField
-        return context
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseForbidden
 
 # Login View
 class UserLoginView(LoginView):
@@ -48,7 +17,7 @@ class UserLogoutView(LogoutView):
 
 # User Registration View
 class UserRegisterView(CreateView):
-    form_class = UserCreationForm()  # Specify the form class for registration
+    form_class = UserCreationForm  # Specify the form class for registration
     template_name = 'relationship_app/register.html'  # Specify the template for registration
     success_url = reverse_lazy('login')  # Redirect to login page after successful registration
 
@@ -65,6 +34,7 @@ def is_admin(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
+    """This view is only accessible by users with the 'Admin' role."""
     return render(request, 'relationship_app/admin.html')
 
 # Librarian View
@@ -73,8 +43,8 @@ def is_librarian(user):
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
+    """This view is only accessible by users with the 'Librarian' role."""
     return render(request, 'relationship_app/librarian.html')
-
 
 # Member View
 def is_member(user):
@@ -82,4 +52,5 @@ def is_member(user):
 
 @user_passes_test(is_member)
 def member_view(request):
+    """This view is only accessible by users with the 'Member' role."""
     return render(request, 'relationship_app/member.html')
