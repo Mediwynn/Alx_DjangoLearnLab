04d86afa0d44528b577b18084a47d46b973eca60
Mediwynn M Maart <mediwynnm@gmail.com>
2024-08-18 17:53:25 +0200

    admin to Admin

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -35,7 +35,7 @@ def is_admin(user):
 @user_passes_test(is_admin)
 def admin_view(request):
     """This view is only accessible by users with the 'Admin' role."""
-    return render(request, 'relationship_app/admin.html')
+    return render(request, 'relationship_app/Admin.html')
 
 # Librarian View
 def is_librarian(user):
