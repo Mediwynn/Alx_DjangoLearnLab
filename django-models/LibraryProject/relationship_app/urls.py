38b20c293a987c9a2a948f6ffe949e7dd3015c59
Mediwynn M Maart <mediwynnm@gmail.com>
2024-08-18 19:55:44 +0200

    admin to admin_view

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for LibraryProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from relationship_app import views
+from relationship_app.views import LibraryDetailView
+from .views import list_books
+from django.urls import path
+from .views import UserLoginView, UserLogoutView, UserRegisterView
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('login/', UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', UserRegisterView.as_view(template_name='relationship_app/register.html'), name='views.register'),
+    path('admin/', admin_view, name='admin'),
+    path('librarian/', librarian_view, name='librarian'),
+    path('member/', member_view, name='member'),
+]
+
