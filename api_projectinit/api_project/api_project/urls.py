b79d67e475cc9e918bc91d8c22f86b40799fccfa
Mediwynn M Maart <mediwynnm@gmail.com>
2024-08-26 19:11:51 +0200

    add url path

--- api_projectinit/api_project/api_project/urls.py
+++ api_projectinit/api_project/api_project/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.views import BookList
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', BookList.as_view(), name='book-list'),
+    path('api/', include('api.urls')),
 ]
-
 
