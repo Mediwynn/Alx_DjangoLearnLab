edcbc5bd67ab3a134cf788b46428e3d5fa047ff2
Mediwynn M Maart <mediwynnm@gmail.com>
2024-08-27 01:26:24 +0200

    authentication

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from api.views import BookList
 from django.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', BookList.as_view(), name='book-list'),
     path('api/', include('api.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
