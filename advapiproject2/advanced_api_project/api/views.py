11001840ad928829fba51812815b193091854c4e
Mediwynn M Maart <mediwynnm@gmail.com>
2024-09-03 02:46:43 +0200

    add api.url

--- advapiproject2/advanced_api_project/api/views.py
+++ advapiproject2/advanced_api_project/api/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework import generics, filters
+from api.models import Book
+from api.serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 # BookListView handles GET (list all books) and POST (create a new book).
 # Read access is open to all, but creating a new book requires authentication.
 # ListView: Handles retrieving all books
-class BookListView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 # BookDetailView handles GET (retrieve a book), PUT/PATCH (update a book), and DELETE (remove a book).
 # Only authenticated users can modify or delete books, but all users can view them.
@@ -41,6 +45,12 @@ class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+
+
+# BookListView handles retrieving a list of books with advanced querying capabilities.
+# Users can filter by title, author name, and publication year.
+# They can also search across the title and author fields and order results by title or publication year.
 
 
 
