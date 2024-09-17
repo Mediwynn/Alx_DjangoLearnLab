9260c7ce215d3148dd9c732aafcf992bf96ddf96
Mediwynn M Maart <mediwynnm@gmail.com>
2024-09-18 01:36:17 +0200

    error 404

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token  # Import Token for generating tokens
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -11,7 +12,30 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # Use get_user_model().objects.create_user() to create the user
+        # Create the user using get_user_model().objects.create_user()
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        
+        # Generate a token for the user
+        Token.objects.create(user=user)
+        
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,25 +6,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import get_user_model
+from accounts.serializers import UserSerializer
+
 
 # RegisterView for user registration
-class RegisterView(generics.GenericAPIView):
-    serializer_class = UserRegistrationSerializer
-
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()  # Save the user using the serializer's create() method
-
-        # Generate token for the user
-        token, created = Token.objects.get_or_create(user=user)
-
-        return Response({
-            "token": token.key,
-            "username": user.username,
-            "email": user.email,
-            "message": "Registration successful"
-        }, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+                'message': 'Registration successful'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # LoginView for user login and token retrieval
